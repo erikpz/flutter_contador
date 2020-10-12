2827d774de32a099dac2e03397d95575e2ffd2b3
Erik Pizaña <erikpzcanedo@outlook.com>
2020-10-12 15:45:08 -0500

    Adding appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('Contador flutter'),
+            ),
+            backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+          ),
+        ));
+  }
+}
